d54622fb2eb095daeb05fbae72b9c7208181fc16
Ege Oral <egeoral@gmail.com>
2022-08-18 15:11:42 +0300

    Score board added.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,13 +8,29 @@ public class Enemy : MonoBehaviour
 
     // Make a stack of vfx
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
+
+    ScoreBoard scoreBoard;
+    private void Start() 
+    {
+       scoreBoard = FindObjectOfType<ScoreBoard>();    
+    }
  
     private void OnParticleCollision(GameObject other) 
+    {
+        ProcessHit();
+        DestroyEnemy();
+    }
+
+    private void ProcessHit()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+    
+    private void DestroyEnemy()
     {
         GameObject vfx = Instantiate(enemeyExplosionVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         Destroy(gameObject);
     }
-
-
 }
