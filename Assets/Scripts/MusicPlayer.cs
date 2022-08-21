95654fe05057434aed49071ba1a70a616c59c941
Ege Oral <egeoral@gmail.com>
2022-08-21 20:40:47 +0300

    Music added.

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake() 
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        int numMusicPlayer = FindObjectsOfType<MusicPlayer>().Length;
+        if(numMusicPlayer > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
     }
 }
